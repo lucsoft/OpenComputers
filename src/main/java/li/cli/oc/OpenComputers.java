cf32ee11bd77f64a86402cb2e396fb24d276018d
lucsoft <mail@lucsoft.de>
2020-12-03 00:34:25 +0100

    1.16 Migration: Added many items back (all items that are for crafting only)

--- src/main/java/li/cli/oc/OpenComputers.java
+++ src/main/java/li/cli/oc/OpenComputers.java
@@ -1,14 +1,21 @@
 package li.cli.oc;
 
+import li.cli.oc.items.Components;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
 
 public class OpenComputers implements ModInitializer {
+
+	public static final String modId = "opencomputers";
+
+	public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(new Identifier(modId, "general"), () -> new ItemStack(Blocks.NETHERITE_BLOCK));
+
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		System.out.println("Hello Fabric world!");
+		Components.registerComponents();
 	}
 }
