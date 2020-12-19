89777f54e192fdcb68587892e828c7ee0d81108a
lucsoft <mail@lucsoft.de>
2020-12-19 20:53:25 +0100

    Fix for #6: Add a OutlineShape to Hologram Added nonOpaque

--- src/main/kotlin/li/cli/oc/blocks/Hologram.kt
+++ src/main/kotlin/li/cli/oc/blocks/Hologram.kt
@@ -4,11 +4,25 @@ import li.cli.oc.blocks.commons.TecBlock
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.minecraft.block.BlockState
 import net.minecraft.block.Material
+import net.minecraft.block.ShapeContext
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.shape.VoxelShape
+import net.minecraft.util.shape.VoxelShapes
+import net.minecraft.world.BlockView
 
 
-class Hologram: TecBlock(FabricBlockSettings.of(Material.METAL)) {
+class Hologram: TecBlock(FabricBlockSettings.of(Material.METAL).nonOpaque()) {
 
     override fun emitsRedstonePower(state: BlockState?): Boolean {
         return false
+    }
+
+    override fun getOutlineShape(
+        state: BlockState?,
+        world: BlockView?,
+        pos: BlockPos?,
+        context: ShapeContext?
+    ): VoxelShape {
+        return VoxelShapes.cuboid(0.0, 0.0, 0.0, 1.0, 0.4, 1.0)
     }
 }
