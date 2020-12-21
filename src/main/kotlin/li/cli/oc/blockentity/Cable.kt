7c800ca6a47097fd8b0d500bda5dc3b8a46ba1b6
lucsoft <mail@lucsoft.de>
2020-12-21 16:24:23 +0100

    #16 Cables now have a nice fitting outlineshape

---
+++ src/main/kotlin/li/cli/oc/blockentity/Cable.kt
@@ -0,0 +1,35 @@
+package li.cli.oc.blockentity
+
+import li.cli.oc.Components
+import li.cli.oc.blocks.Cable
+import li.cli.oc.blocks.commons.TecBlock
+import net.minecraft.block.entity.BlockEntity
+import net.minecraft.util.math.Direction
+import net.minecraft.util.math.Vec3i
+
+class Cable: BlockEntity(Components.BlockEntities.Cable.entityType) {
+
+    var connections: MutableList<Direction>? = null
+
+    fun updateConnected(): MutableList<Direction> {
+        val checkForCable = listOf(
+            Vec3i(0,-1,0), Vec3i(0,1,0),
+            Vec3i(0,0,-1), Vec3i(0,0,1),
+            Vec3i(-1,0,0), Vec3i(1,0,0)
+        )
+        val connection = mutableListOf<Direction>()
+        checkForCable.forEachIndexed { index, vec3i ->
+            val blockstate = world!!.getBlockState(pos?.add(vec3i))
+            val block = blockstate.block
+            val direction =  Direction.byId(index)
+            if(block is Cable)
+                connection.add(direction)
+            else if(block is TecBlock && direction.opposite in block.allowedToBeConnected(blockstate)) {
+                connection.add(direction)
+            }
+        }
+
+        connections = connection;
+        return connection;
+    }
+}
